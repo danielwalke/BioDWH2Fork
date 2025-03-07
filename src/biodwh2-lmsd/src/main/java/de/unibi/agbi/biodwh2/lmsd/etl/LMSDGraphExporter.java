45bbd1100031774fe6a35524e865d03b47af79c6
Marcel Friedrichs <astror.enales@gmail.com>
2025-03-07 12:57:05 +0100

    Export LMSD classification hierarchy

--- src/biodwh2-lmsd/src/main/java/de/unibi/agbi/biodwh2/lmsd/etl/LMSDGraphExporter.java
+++ src/biodwh2-lmsd/src/main/java/de/unibi/agbi/biodwh2/lmsd/etl/LMSDGraphExporter.java
@@ -8,6 +8,8 @@ import de.unibi.agbi.biodwh2.core.io.FileUtils;
 import de.unibi.agbi.biodwh2.core.io.sdf.SdfEntry;
 import de.unibi.agbi.biodwh2.core.io.sdf.SdfReader;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
+import de.unibi.agbi.biodwh2.core.model.graph.IndexDescription;
+import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.lmsd.LMSDDataSource;
 import org.apache.commons.lang3.StringUtils;
 
@@ -15,6 +17,7 @@ import java.nio.charset.StandardCharsets;
 
 public class LMSDGraphExporter extends GraphExporter<LMSDDataSource> {
     static final String LIPID_LABEL = "Lipid";
+    static final String LIPID_CLASSIFICATION_LABEL = "LipidClassification";
 
     public LMSDGraphExporter(final LMSDDataSource dataSource) {
         super(dataSource);
@@ -27,6 +30,8 @@ public class LMSDGraphExporter extends GraphExporter<LMSDDataSource> {
 
     @Override
     protected boolean exportGraph(final Workspace workspace, final Graph graph) throws ExporterException {
+        graph.addIndex(IndexDescription.forNode(LIPID_LABEL, ID_KEY, IndexDescription.Type.UNIQUE));
+        graph.addIndex(IndexDescription.forNode(LIPID_CLASSIFICATION_LABEL, ID_KEY, IndexDescription.Type.UNIQUE));
         try {
             FileUtils.forEachZipEntry(workspace, dataSource, LMSDUpdater.FILE_NAME, ".sdf", (stream, zipEntry) -> {
                 final var reader = new SdfReader(stream, StandardCharsets.UTF_8);
@@ -48,13 +53,9 @@ public class LMSDGraphExporter extends GraphExporter<LMSDDataSource> {
         builder.withPropertyIfNotNull("formula", entry.properties.get("FORMULA"));
         builder.withPropertyIfNotNull("name", entry.properties.get("NAME"));
         builder.withPropertyIfNotNull("abbreviation", entry.properties.get("ABBREVIATION"));
-        builder.withPropertyIfNotNull("main_class", entry.properties.get("MAIN_CLASS"));
-        builder.withPropertyIfNotNull("sub_class", entry.properties.get("SUB_CLASS"));
         builder.withPropertyIfNotNull("inchi", entry.properties.get("INCHI"));
         builder.withPropertyIfNotNull("inchi_key", entry.properties.get("INCHI_KEY"));
         builder.withPropertyIfNotNull("systematic_name", entry.properties.get("SYSTEMATIC_NAME"));
-        builder.withPropertyIfNotNull("category", entry.properties.get("CATEGORY"));
-        builder.withPropertyIfNotNull("class_level4", entry.properties.get("CLASS_LEVEL4"));
         final String synonyms = entry.properties.get("SYNONYMS");
         if (synonyms != null)
             builder.withPropertyIfNotNull("synonyms", StringUtils.splitByWholeSeparator(synonyms, "; "));
@@ -65,6 +66,56 @@ public class LMSDGraphExporter extends GraphExporter<LMSDDataSource> {
         builder.withPropertyIfNotNull("swisslipids_id", entry.properties.get("SWISSLIPIDS_ID"));
         builder.withPropertyIfNotNull("lipidbank_id", entry.properties.get("LIPIDBANK_ID"));
         builder.withPropertyIfNotNull("kegg_id", entry.properties.get("KEGG_ID"));
-        builder.build();
+        final Node node = builder.build();
+        final Classification categoryId = parseClassification(entry.properties.get("CATEGORY"));
+        final Classification mainClassId = parseClassification(entry.properties.get("MAIN_CLASS"));
+        final Classification subClassId = parseClassification(entry.properties.get("SUB_CLASS"));
+        final Classification classLevel4Id = parseClassification(entry.properties.get("CLASS_LEVEL4"));
+        Long categoryNode = graph.findNodeId(LIPID_CLASSIFICATION_LABEL, ID_KEY, categoryId.id);
+        if (categoryNode == null)
+            categoryNode = graph.addNode(LIPID_CLASSIFICATION_LABEL, ID_KEY, categoryId.id, "name", categoryId.name)
+                                .getId();
+        Long mainClassNode = graph.findNodeId(LIPID_CLASSIFICATION_LABEL, ID_KEY, mainClassId.id);
+        if (mainClassNode == null) {
+            mainClassNode = graph.addNode(LIPID_CLASSIFICATION_LABEL, ID_KEY, mainClassId.id, "name", mainClassId.name)
+                                 .getId();
+            graph.addEdge(mainClassNode, categoryNode, "CHILD_OF");
+        }
+        if (subClassId != null) {
+            Long subClassNode = graph.findNodeId(LIPID_CLASSIFICATION_LABEL, ID_KEY, subClassId.id);
+            if (subClassNode == null) {
+                subClassNode = graph.addNode(LIPID_CLASSIFICATION_LABEL, ID_KEY, subClassId.id, "name", subClassId.name)
+                                    .getId();
+                graph.addEdge(subClassNode, mainClassNode, "CHILD_OF");
+            }
+            if (classLevel4Id != null) {
+                Long classLevel4Node = graph.findNodeId(LIPID_CLASSIFICATION_LABEL, ID_KEY, classLevel4Id.id);
+                if (classLevel4Node == null) {
+                    classLevel4Node = graph.addNode(LIPID_CLASSIFICATION_LABEL, ID_KEY, classLevel4Id.id, "name",
+                                                    classLevel4Id.name).getId();
+                    graph.addEdge(classLevel4Node, subClassNode, "CHILD_OF");
+                }
+                graph.addEdge(node, classLevel4Node, "BELONGS_TO");
+            } else {
+                graph.addEdge(node, subClassNode, "BELONGS_TO");
+            }
+        } else {
+            graph.addEdge(node, mainClassNode, "BELONGS_TO");
+        }
+    }
+
+    private Classification parseClassification(final String value) {
+        if (value == null)
+            return null;
+        final String[] parts = StringUtils.splitByWholeSeparator(value, " [", 2);
+        final var classification = new Classification();
+        classification.id = parts[1].replace("]", "").trim();
+        classification.name = parts[0].trim();
+        return classification;
+    }
+
+    private static class Classification {
+        public String id;
+        public String name;
     }
 }
