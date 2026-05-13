8ae69f3aa98b7592e0372b1dac5af7ddbc2b83c2
danielwalke <daniel.walke@ovgu.de>
2026-05-13 18:05:29 +0200

    new test

--- test_start.sh
+++ test_start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 git pull origin main
 if git diff --name-only ORIG_HEAD HEAD | grep -q "^version\.md$"; then
-	echo "Hello it runs Version 4"
-	git add start.log
-	git commit -m "new logs"
-	git push
-	echo "Finished?"
+	echo "Hello it runs Version 4" >> start.log
+	echo "Finished?" >> start.log
 fi
