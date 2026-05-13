a4261bdb3c5acf31da106d1da44c6cc7bf0a6eea
danielwalke <daniel.walke@ovgu.de>
2026-05-13 18:15:14 +0200

    new test

--- test_start.sh
+++ test_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull origin main
 echo "Pulled" >> start.plog
-if git diff --name-only ORIG_HEAD HEAD | grep -q "^version\.md$"; then
-	echo "Hello it runs Version 6" >> start.plog
+if git diff --name-only HEAD~1 HEAD | grep -q "^version\.md$"; then
+	echo "Hello it runs Version 7" >> start.plog
 	echo "Finished!" >> start.plog
 fi
