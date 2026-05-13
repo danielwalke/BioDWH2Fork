e2408a5dabf07a373a205156fc335d92b7426f38
danielwalke <daniel.walke@ovgu.de>
2026-05-13 18:07:49 +0200

    new test

--- test_start.sh
+++ test_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull origin main
 if git diff --name-only ORIG_HEAD HEAD | grep -q "^version\.md$"; then
-	echo "Hello it runs Version 4" >> start.log
-	echo "Finished?" >> start.log
+	echo "Hello it runs Version 5" >> start.log
+	echo "Finished!" >> start.log
 fi
