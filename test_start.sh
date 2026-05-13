5840e5819345257a2f74eb4d5441e41fdd22b6b4
danielwalke <daniel.walke@ovgu.de>
2026-05-13 18:23:22 +0200

    final test

--- test_start.sh
+++ test_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull origin main
 echo "Pulled" >> start.plog
-echo "Hello it runs Version 7" >> start.plog
+echo "Hello it runs Version 8" >> start.plog
 echo "Finished!" >> start.plog
