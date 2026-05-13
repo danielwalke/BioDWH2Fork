e922ae38cdc902f43c37cceb92e1fc45bd4933b0
danielwalke <daniel.walke@ovgu.de>
2026-05-13 17:54:52 +0200

    test for runners

--- test_start.sh
+++ test_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull origin main
 if git diff --name-only ORIG_HEAD HEAD | grep -q "^version\.md$"; then
-	echo "Hello it runs"
+	echo "Hello it runs Version 2"
 	git add start.log
 	git commit -m "new logs"
 	git push
